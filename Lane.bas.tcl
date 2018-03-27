c0a42059b72ac36051d8b5a797f5381f0fc48f40
jspenliany <jspenliany@users.noreply.github.com>
2018-03-27 17:03:22 +0800

    Add files via upload

--- Lane.bas.tcl
+++ Lane.bas.tcl
@@ -1,74 +1,66 @@
 
-$nodeb_(0) set X_ 87
-$nodeb_(0) set Y_ 79.2
+$nodeb_(0) set X_ 3
+$nodeb_(0) set Y_ 3
 $nodeb_(0) set Z_ 0
 $nodeb_(0) color "green"
-$nodeb_(1) set X_ 1065.5
-$nodeb_(1) set Y_ 79.2
+$nodeb_(1) set X_ 769.667
+$nodeb_(1) set Y_ 3
 $nodeb_(1) set Z_ 0
 $nodeb_(1) color "green"
-$nodeb_(2) set X_ 2044
-$nodeb_(2) set Y_ 79.2
+$nodeb_(2) set X_ 1536.33
+$nodeb_(2) set Y_ 3
 $nodeb_(2) set Z_ 0
 $nodeb_(2) color "green"
-$nodeb_(3) set X_ 87
-$nodeb_(3) set Y_ 360.733
+$nodeb_(3) set X_ 2303
+$nodeb_(3) set Y_ 3
 $nodeb_(3) set Z_ 0
 $nodeb_(3) color "green"
-$nodeb_(4) set X_ 1065.5
-$nodeb_(4) set Y_ 360.733
+$nodeb_(4) set X_ 3
+$nodeb_(4) set Y_ 769.667
 $nodeb_(4) set Z_ 0
 $nodeb_(4) color "green"
-$nodeb_(5) set X_ 2044
-$nodeb_(5) set Y_ 360.733
+$nodeb_(5) set X_ 769.667
+$nodeb_(5) set Y_ 769.667
 $nodeb_(5) set Z_ 0
 $nodeb_(5) color "green"
-$nodeb_(6) set X_ 87
-$nodeb_(6) set Y_ 642.267
+$nodeb_(6) set X_ 1536.33
+$nodeb_(6) set Y_ 769.667
 $nodeb_(6) set Z_ 0
 $nodeb_(6) color "green"
-$nodeb_(7) set X_ 1065.5
-$nodeb_(7) set Y_ 642.267
+$nodeb_(7) set X_ 2303
+$nodeb_(7) set Y_ 769.667
 $nodeb_(7) set Z_ 0
 $nodeb_(7) color "green"
-$nodeb_(8) set X_ 2044
-$nodeb_(8) set Y_ 642.267
+$nodeb_(8) set X_ 3
+$nodeb_(8) set Y_ 1536.33
 $nodeb_(8) set Z_ 0
 $nodeb_(8) color "green"
-$nodeb_(9) set X_ 87
-$nodeb_(9) set Y_ 923.8
+$nodeb_(9) set X_ 769.667
+$nodeb_(9) set Y_ 1536.33
 $nodeb_(9) set Z_ 0
 $nodeb_(9) color "green"
-$nodeb_(10) set X_ 1065.5
-$nodeb_(10) set Y_ 923.8
+$nodeb_(10) set X_ 1536.33
+$nodeb_(10) set Y_ 1536.33
 $nodeb_(10) set Z_ 0
 $nodeb_(10) color "green"
-$nodeb_(11) set X_ 2044
-$nodeb_(11) set Y_ 923.8
+$nodeb_(11) set X_ 2303
+$nodeb_(11) set Y_ 1536.33
 $nodeb_(11) set Z_ 0
 $nodeb_(11) color "green"
-$nodeb_(12) set X_ 576.25
-$nodeb_(12) set Y_ 79.2
+$nodeb_(12) set X_ 3
+$nodeb_(12) set Y_ 2303
 $nodeb_(12) set Z_ 0
 $nodeb_(12) color "green"
-$nodeb_(13) set X_ 1554.75
-$nodeb_(13) set Y_ 79.2
+$nodeb_(13) set X_ 769.667
+$nodeb_(13) set Y_ 2303
 $nodeb_(13) set Z_ 0
 $nodeb_(13) color "green"
-$nodeb_(14) set X_ 576.25
-$nodeb_(14) set Y_ 360.733
+$nodeb_(14) set X_ 1536.33
+$nodeb_(14) set Y_ 2303
 $nodeb_(14) set Z_ 0
 $nodeb_(14) color "green"
-$nodeb_(15) set X_ 1554.75
-$nodeb_(15) set Y_ 360.733
+$nodeb_(15) set X_ 2303
+$nodeb_(15) set Y_ 2303
 $nodeb_(15) set Z_ 0
 $nodeb_(15) color "green"
-$nodeb_(16) set X_ 576.25
-$nodeb_(16) set Y_ 642.267
-$nodeb_(16) set Z_ 0
-$nodeb_(16) color "green"
-$nodeb_(17) set X_ 1554.75
-$nodeb_(17) set Y_ 642.267
-$nodeb_(17) set Z_ 0
-$nodeb_(17) color "green"
 
---
+++ Lane.sce.tcl
@@ -0,0 +1,17 @@
+
+set opt(nm)		10		;#  number of mobilenodes
+set opt(nb)		16		;#  number of base stations
+set opt(nw)		16		;#  number of wired nodes
+set opt(x)		2306		;# X dimension of topography
+set opt(y)		2306		;# Y dimension of topography
+set opt(rowc)		4		;# Y division No.
+set opt(colc)		4		;# X division No.
+set opt(nodesXx)		0		;# no. of nodes add between X
+set opt(nodesYy)		0		;# no. of nodes add between Y
+set opt(distnodesXx)		0		;# Distance between no. of nodes add between X
+set opt(distnodesYy)		0		;# Distance between no. of nodes add between Y
+set opt(juncDelayXx)		0.749997		;# delay between no. of nodes add between X
+set opt(juncDelayYy)		0.749997		;# delay between no. of nodes add between Y
+set opt(baseDelayXx)		0.0144374		;# delay between no. of nodes add between X
+set opt(baseDelayYy)		0.0144374		;# delay between no. of nodes add between Y
+
