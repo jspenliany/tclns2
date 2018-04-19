e61b6e9c9782165aa35786c39433d7a0af6681a5
jspenliany <jspenliany@users.noreply.github.com>
2018-04-19 23:10:01 +0800

    Add files via upload

--- Lane.bas.tcl
+++ Lane.bas.tcl
@@ -1,74 +1,326 @@
 
-$nodeb_(0) set X_ 87
-$nodeb_(0) set Y_ 79.2
+$nodeb_(0) set X_ 3
+$nodeb_(0) set Y_ 3
 $nodeb_(0) set Z_ 0
 $nodeb_(0) color "green"
-$nodeb_(1) set X_ 1065.5
-$nodeb_(1) set Y_ 79.2
+$nodeb_(1) set X_ 565.5
+$nodeb_(1) set Y_ 3
 $nodeb_(1) set Z_ 0
 $nodeb_(1) color "green"
-$nodeb_(2) set X_ 2044
-$nodeb_(2) set Y_ 79.2
+$nodeb_(2) set X_ 1128
+$nodeb_(2) set Y_ 3
 $nodeb_(2) set Z_ 0
 $nodeb_(2) color "green"
-$nodeb_(3) set X_ 87
-$nodeb_(3) set Y_ 360.733
+$nodeb_(3) set X_ 1690.5
+$nodeb_(3) set Y_ 3
 $nodeb_(3) set Z_ 0
 $nodeb_(3) color "green"
-$nodeb_(4) set X_ 1065.5
-$nodeb_(4) set Y_ 360.733
+$nodeb_(4) set X_ 2253
+$nodeb_(4) set Y_ 3
 $nodeb_(4) set Z_ 0
 $nodeb_(4) color "green"
-$nodeb_(5) set X_ 2044
-$nodeb_(5) set Y_ 360.733
+$nodeb_(5) set X_ 2815.5
+$nodeb_(5) set Y_ 3
 $nodeb_(5) set Z_ 0
 $nodeb_(5) color "green"
-$nodeb_(6) set X_ 87
-$nodeb_(6) set Y_ 642.267
+$nodeb_(6) set X_ 3378
+$nodeb_(6) set Y_ 3
 $nodeb_(6) set Z_ 0
 $nodeb_(6) color "green"
-$nodeb_(7) set X_ 1065.5
-$nodeb_(7) set Y_ 642.267
+$nodeb_(7) set X_ 3940.5
+$nodeb_(7) set Y_ 3
 $nodeb_(7) set Z_ 0
 $nodeb_(7) color "green"
-$nodeb_(8) set X_ 2044
-$nodeb_(8) set Y_ 642.267
+$nodeb_(8) set X_ 4503
+$nodeb_(8) set Y_ 3
 $nodeb_(8) set Z_ 0
 $nodeb_(8) color "green"
-$nodeb_(9) set X_ 87
-$nodeb_(9) set Y_ 923.8
+$nodeb_(9) set X_ 3
+$nodeb_(9) set Y_ 565.5
 $nodeb_(9) set Z_ 0
 $nodeb_(9) color "green"
-$nodeb_(10) set X_ 1065.5
-$nodeb_(10) set Y_ 923.8
+$nodeb_(10) set X_ 565.5
+$nodeb_(10) set Y_ 565.5
 $nodeb_(10) set Z_ 0
 $nodeb_(10) color "green"
-$nodeb_(11) set X_ 2044
-$nodeb_(11) set Y_ 923.8
+$nodeb_(11) set X_ 1128
+$nodeb_(11) set Y_ 565.5
 $nodeb_(11) set Z_ 0
 $nodeb_(11) color "green"
-$nodeb_(12) set X_ 576.25
-$nodeb_(12) set Y_ 79.2
+$nodeb_(12) set X_ 1690.5
+$nodeb_(12) set Y_ 565.5
 $nodeb_(12) set Z_ 0
 $nodeb_(12) color "green"
-$nodeb_(13) set X_ 1554.75
-$nodeb_(13) set Y_ 79.2
+$nodeb_(13) set X_ 2253
+$nodeb_(13) set Y_ 565.5
 $nodeb_(13) set Z_ 0
 $nodeb_(13) color "green"
-$nodeb_(14) set X_ 576.25
-$nodeb_(14) set Y_ 360.733
+$nodeb_(14) set X_ 2815.5
+$nodeb_(14) set Y_ 565.5
 $nodeb_(14) set Z_ 0
 $nodeb_(14) color "green"
-$nodeb_(15) set X_ 1554.75
-$nodeb_(15) set Y_ 360.733
+$nodeb_(15) set X_ 3378
+$nodeb_(15) set Y_ 565.5
 $nodeb_(15) set Z_ 0
 $nodeb_(15) color "green"
-$nodeb_(16) set X_ 576.25
-$nodeb_(16) set Y_ 642.267
+$nodeb_(16) set X_ 3940.5
+$nodeb_(16) set Y_ 565.5
 $nodeb_(16) set Z_ 0
 $nodeb_(16) color "green"
-$nodeb_(17) set X_ 1554.75
-$nodeb_(17) set Y_ 642.267
+$nodeb_(17) set X_ 4503
+$nodeb_(17) set Y_ 565.5
 $nodeb_(17) set Z_ 0
 $nodeb_(17) color "green"
+$nodeb_(18) set X_ 3
+$nodeb_(18) set Y_ 1128
+$nodeb_(18) set Z_ 0
+$nodeb_(18) color "green"
+$nodeb_(19) set X_ 565.5
+$nodeb_(19) set Y_ 1128
+$nodeb_(19) set Z_ 0
+$nodeb_(19) color "green"
+$nodeb_(20) set X_ 1128
+$nodeb_(20) set Y_ 1128
+$nodeb_(20) set Z_ 0
+$nodeb_(20) color "green"
+$nodeb_(21) set X_ 1690.5
+$nodeb_(21) set Y_ 1128
+$nodeb_(21) set Z_ 0
+$nodeb_(21) color "green"
+$nodeb_(22) set X_ 2253
+$nodeb_(22) set Y_ 1128
+$nodeb_(22) set Z_ 0
+$nodeb_(22) color "green"
+$nodeb_(23) set X_ 2815.5
+$nodeb_(23) set Y_ 1128
+$nodeb_(23) set Z_ 0
+$nodeb_(23) color "green"
+$nodeb_(24) set X_ 3378
+$nodeb_(24) set Y_ 1128
+$nodeb_(24) set Z_ 0
+$nodeb_(24) color "green"
+$nodeb_(25) set X_ 3940.5
+$nodeb_(25) set Y_ 1128
+$nodeb_(25) set Z_ 0
+$nodeb_(25) color "green"
+$nodeb_(26) set X_ 4503
+$nodeb_(26) set Y_ 1128
+$nodeb_(26) set Z_ 0
+$nodeb_(26) color "green"
+$nodeb_(27) set X_ 3
+$nodeb_(27) set Y_ 1690.5
+$nodeb_(27) set Z_ 0
+$nodeb_(27) color "green"
+$nodeb_(28) set X_ 565.5
+$nodeb_(28) set Y_ 1690.5
+$nodeb_(28) set Z_ 0
+$nodeb_(28) color "green"
+$nodeb_(29) set X_ 1128
+$nodeb_(29) set Y_ 1690.5
+$nodeb_(29) set Z_ 0
+$nodeb_(29) color "green"
+$nodeb_(30) set X_ 1690.5
+$nodeb_(30) set Y_ 1690.5
+$nodeb_(30) set Z_ 0
+$nodeb_(30) color "green"
+$nodeb_(31) set X_ 2253
+$nodeb_(31) set Y_ 1690.5
+$nodeb_(31) set Z_ 0
+$nodeb_(31) color "green"
+$nodeb_(32) set X_ 2815.5
+$nodeb_(32) set Y_ 1690.5
+$nodeb_(32) set Z_ 0
+$nodeb_(32) color "green"
+$nodeb_(33) set X_ 3378
+$nodeb_(33) set Y_ 1690.5
+$nodeb_(33) set Z_ 0
+$nodeb_(33) color "green"
+$nodeb_(34) set X_ 3940.5
+$nodeb_(34) set Y_ 1690.5
+$nodeb_(34) set Z_ 0
+$nodeb_(34) color "green"
+$nodeb_(35) set X_ 4503
+$nodeb_(35) set Y_ 1690.5
+$nodeb_(35) set Z_ 0
+$nodeb_(35) color "green"
+$nodeb_(36) set X_ 3
+$nodeb_(36) set Y_ 2253
+$nodeb_(36) set Z_ 0
+$nodeb_(36) color "green"
+$nodeb_(37) set X_ 565.5
+$nodeb_(37) set Y_ 2253
+$nodeb_(37) set Z_ 0
+$nodeb_(37) color "green"
+$nodeb_(38) set X_ 1128
+$nodeb_(38) set Y_ 2253
+$nodeb_(38) set Z_ 0
+$nodeb_(38) color "green"
+$nodeb_(39) set X_ 1690.5
+$nodeb_(39) set Y_ 2253
+$nodeb_(39) set Z_ 0
+$nodeb_(39) color "green"
+$nodeb_(40) set X_ 2253
+$nodeb_(40) set Y_ 2253
+$nodeb_(40) set Z_ 0
+$nodeb_(40) color "green"
+$nodeb_(41) set X_ 2815.5
+$nodeb_(41) set Y_ 2253
+$nodeb_(41) set Z_ 0
+$nodeb_(41) color "green"
+$nodeb_(42) set X_ 3378
+$nodeb_(42) set Y_ 2253
+$nodeb_(42) set Z_ 0
+$nodeb_(42) color "green"
+$nodeb_(43) set X_ 3940.5
+$nodeb_(43) set Y_ 2253
+$nodeb_(43) set Z_ 0
+$nodeb_(43) color "green"
+$nodeb_(44) set X_ 4503
+$nodeb_(44) set Y_ 2253
+$nodeb_(44) set Z_ 0
+$nodeb_(44) color "green"
+$nodeb_(45) set X_ 3
+$nodeb_(45) set Y_ 2815.5
+$nodeb_(45) set Z_ 0
+$nodeb_(45) color "green"
+$nodeb_(46) set X_ 565.5
+$nodeb_(46) set Y_ 2815.5
+$nodeb_(46) set Z_ 0
+$nodeb_(46) color "green"
+$nodeb_(47) set X_ 1128
+$nodeb_(47) set Y_ 2815.5
+$nodeb_(47) set Z_ 0
+$nodeb_(47) color "green"
+$nodeb_(48) set X_ 1690.5
+$nodeb_(48) set Y_ 2815.5
+$nodeb_(48) set Z_ 0
+$nodeb_(48) color "green"
+$nodeb_(49) set X_ 2253
+$nodeb_(49) set Y_ 2815.5
+$nodeb_(49) set Z_ 0
+$nodeb_(49) color "green"
+$nodeb_(50) set X_ 2815.5
+$nodeb_(50) set Y_ 2815.5
+$nodeb_(50) set Z_ 0
+$nodeb_(50) color "green"
+$nodeb_(51) set X_ 3378
+$nodeb_(51) set Y_ 2815.5
+$nodeb_(51) set Z_ 0
+$nodeb_(51) color "green"
+$nodeb_(52) set X_ 3940.5
+$nodeb_(52) set Y_ 2815.5
+$nodeb_(52) set Z_ 0
+$nodeb_(52) color "green"
+$nodeb_(53) set X_ 4503
+$nodeb_(53) set Y_ 2815.5
+$nodeb_(53) set Z_ 0
+$nodeb_(53) color "green"
+$nodeb_(54) set X_ 3
+$nodeb_(54) set Y_ 3378
+$nodeb_(54) set Z_ 0
+$nodeb_(54) color "green"
+$nodeb_(55) set X_ 565.5
+$nodeb_(55) set Y_ 3378
+$nodeb_(55) set Z_ 0
+$nodeb_(55) color "green"
+$nodeb_(56) set X_ 1128
+$nodeb_(56) set Y_ 3378
+$nodeb_(56) set Z_ 0
+$nodeb_(56) color "green"
+$nodeb_(57) set X_ 1690.5
+$nodeb_(57) set Y_ 3378
+$nodeb_(57) set Z_ 0
+$nodeb_(57) color "green"
+$nodeb_(58) set X_ 2253
+$nodeb_(58) set Y_ 3378
+$nodeb_(58) set Z_ 0
+$nodeb_(58) color "green"
+$nodeb_(59) set X_ 2815.5
+$nodeb_(59) set Y_ 3378
+$nodeb_(59) set Z_ 0
+$nodeb_(59) color "green"
+$nodeb_(60) set X_ 3378
+$nodeb_(60) set Y_ 3378
+$nodeb_(60) set Z_ 0
+$nodeb_(60) color "green"
+$nodeb_(61) set X_ 3940.5
+$nodeb_(61) set Y_ 3378
+$nodeb_(61) set Z_ 0
+$nodeb_(61) color "green"
+$nodeb_(62) set X_ 4503
+$nodeb_(62) set Y_ 3378
+$nodeb_(62) set Z_ 0
+$nodeb_(62) color "green"
+$nodeb_(63) set X_ 3
+$nodeb_(63) set Y_ 3940.5
+$nodeb_(63) set Z_ 0
+$nodeb_(63) color "green"
+$nodeb_(64) set X_ 565.5
+$nodeb_(64) set Y_ 3940.5
+$nodeb_(64) set Z_ 0
+$nodeb_(64) color "green"
+$nodeb_(65) set X_ 1128
+$nodeb_(65) set Y_ 3940.5
+$nodeb_(65) set Z_ 0
+$nodeb_(65) color "green"
+$nodeb_(66) set X_ 1690.5
+$nodeb_(66) set Y_ 3940.5
+$nodeb_(66) set Z_ 0
+$nodeb_(66) color "green"
+$nodeb_(67) set X_ 2253
+$nodeb_(67) set Y_ 3940.5
+$nodeb_(67) set Z_ 0
+$nodeb_(67) color "green"
+$nodeb_(68) set X_ 2815.5
+$nodeb_(68) set Y_ 3940.5
+$nodeb_(68) set Z_ 0
+$nodeb_(68) color "green"
+$nodeb_(69) set X_ 3378
+$nodeb_(69) set Y_ 3940.5
+$nodeb_(69) set Z_ 0
+$nodeb_(69) color "green"
+$nodeb_(70) set X_ 3940.5
+$nodeb_(70) set Y_ 3940.5
+$nodeb_(70) set Z_ 0
+$nodeb_(70) color "green"
+$nodeb_(71) set X_ 4503
+$nodeb_(71) set Y_ 3940.5
+$nodeb_(71) set Z_ 0
+$nodeb_(71) color "green"
+$nodeb_(72) set X_ 3
+$nodeb_(72) set Y_ 4503
+$nodeb_(72) set Z_ 0
+$nodeb_(72) color "green"
+$nodeb_(73) set X_ 565.5
+$nodeb_(73) set Y_ 4503
+$nodeb_(73) set Z_ 0
+$nodeb_(73) color "green"
+$nodeb_(74) set X_ 1128
+$nodeb_(74) set Y_ 4503
+$nodeb_(74) set Z_ 0
+$nodeb_(74) color "green"
+$nodeb_(75) set X_ 1690.5
+$nodeb_(75) set Y_ 4503
+$nodeb_(75) set Z_ 0
+$nodeb_(75) color "green"
+$nodeb_(76) set X_ 2253
+$nodeb_(76) set Y_ 4503
+$nodeb_(76) set Z_ 0
+$nodeb_(76) color "green"
+$nodeb_(77) set X_ 2815.5
+$nodeb_(77) set Y_ 4503
+$nodeb_(77) set Z_ 0
+$nodeb_(77) color "green"
+$nodeb_(78) set X_ 3378
+$nodeb_(78) set Y_ 4503
+$nodeb_(78) set Z_ 0
+$nodeb_(78) color "green"
+$nodeb_(79) set X_ 3940.5
+$nodeb_(79) set Y_ 4503
+$nodeb_(79) set Z_ 0
+$nodeb_(79) color "green"
+$nodeb_(80) set X_ 4503
+$nodeb_(80) set Y_ 4503
+$nodeb_(80) set Z_ 0
+$nodeb_(80) color "green"
 
---
+++ Lane.sce.tcl
@@ -0,0 +1,17 @@
+
+set opt(nm)		100		;#  number of mobilenodes
+set opt(nb)		81		;#  number of base stations
+set opt(nw)		81		;#  number of wired nodes
+set opt(x)		4506		;# X dimension of topography
+set opt(y)		4506		;# Y dimension of topography
+set opt(rowc)		9		;# Y division No.
+set opt(colc)		9		;# X division No.
+set opt(nodesXx)		0		;# no. of nodes add between X
+set opt(nodesYy)		0		;# no. of nodes add between Y
+set opt(distnodesXx)		0		;# Distance between no. of nodes add between X
+set opt(distnodesYy)		0		;# Distance between no. of nodes add between Y
+set opt(juncDelayXx)		0.546425		;# delay between no. of nodes add between X
+set opt(juncDelayYy)		0.546425		;# delay between no. of nodes add between Y
+set opt(baseDelayXx)		0.0144374		;# delay between no. of nodes add between X
+set opt(baseDelayYy)		0.0144374		;# delay between no. of nodes add between Y
+
